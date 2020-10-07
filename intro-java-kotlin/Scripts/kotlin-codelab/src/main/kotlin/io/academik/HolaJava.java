c2b722ce738cae4adecc0b63b157c191f7fbd9fd
Víctor Orozco <vorozco@nabenik.com>
2020-10-07 10:39:04 -0600

    Adding regular modifications

--- intro-java-kotlin/Scripts/kotlin-codelab/src/main/kotlin/io/academik/HolaJava.java
+++ intro-java-kotlin/Scripts/kotlin-codelab/src/main/kotlin/io/academik/HolaJava.java
@@ -4,9 +4,5 @@ public class HolaJava {
 
     public static void main(String args[]){
         //Polimorfica
-        Vehiculo t1 = new Tractor();
-        System.out.println(t1.getMarca());
-        System.out.println(t1.getColor());
-        System.out.println(t1.getModelo());
     }
 }
